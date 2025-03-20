5d45dda2e91219811cf721ec1e622c45dbc557ca
Vladimir <anykey196@gmail.com>
2025-03-20 15:06:11 +0500

    задачи

--- mod5/task1.py
+++ mod5/task1.py
@@ -1,39 +1,37 @@
 # Названия файлов
-INPUT_FILE = 'task1_input.txt'
-OUTPUT_FILE = 'task1_output.txt'
-print("Найдите остаток при делении числа a на b.")
-# Открытие файла на чтение
-fi = open(INPUT_FILE, 'r')
-# Чтение данных из файла
-string = fi.read()
-# Закрытие файла
-fi.close()
-# Вывод считанных данных в консоль
-print(f"Исходные данные: {string} ")
-print(type(string))
-# Преобразование строки в список строк с регулярным выражением " "
-c=0
-for count in string:
-    if count == ",":
-        break
-    c=c+1
-print(f"c = {c}")
-print(type(c))
-# Присваивание переменным значений из списка
-a = int(string[:c])
-b = int(string[c+1:])
-# Вывод значений полученных переменных в консоль
-print(f"a = {a}")
-print(type(a))
-print(f"b = {b}")
-print(type(b))
-# Нахождение остатка
-result = a % b
-# Вывод остатка в консоль
-print(f"Остаток: {result}")
-# Запись результата в файл
-fo = open(OUTPUT_FILE, 'w')
-fo.write(str(result))
-fo.close()
-# Вывод в консоль в какой файл осуществлена запись
-print(f"Остаток записан в файл {OUTPUT_FILE}.")
+Задача 1.
+Создать класс. Стек на структуре данных односвязный список. 
+class Node:
+    """
+    Вспомогательный класс для узлов списка
+    """
+    def __init__(self, data):
+        self.data = data  # храним информацию
+        self.pref = None  # ссылка на предыдущий узел
+
+class Stack:
+    """
+    Основной класс для стека
+    """
+
+    def __init__(self):
+        self.end = None  # ссылка на конец стека
+
+    def pop(self):
+        """
+        возвращение последнего элемента из списка с удалением его из списка
+        """
+        pass
+        return val
+
+    def push(self, val):
+        """
+        добавление элемента val в конец списка
+        """
+        pass
+
+    def print(self):
+        """
+        вывод на печать содержимого стека
+        """
+        pass
